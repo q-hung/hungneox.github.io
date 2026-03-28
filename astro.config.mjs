433214e89e5de16b0a05159b3d76d6f8afdeed22
Hung Nguyen <hungnq1989@gmail.com>
2026-03-28 13:43:51 +0200

    add an end post and fix syntax highlingting in dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://butchiso.com',
   integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
+      },
+    },
+  },
   i18n: {
     defaultLocale: 'vi',
     locales: ['vi', 'en'],

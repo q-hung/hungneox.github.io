0391530725edfbfe153890c588af3cc6851c2f2b
Hung Nguyen <hungnq1989@gmail.com>
2026-03-28 14:45:12 +0200

    Change syntax highlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       themes: {
-        light: 'catppuccin-latte',
-        dark: 'catppuccin-mocha',
+        light: 'github-light-default',
+        dark: 'github-dark-dimmed',
       },
     },
   },
